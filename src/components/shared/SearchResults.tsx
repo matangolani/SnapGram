6c7e6d915780b73274ecee1eca28e72d8a856e5f
matangolani <matgolani@gmail.com>
2024-03-19 13:33:05 +0000

    Fixed more errors

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -4,12 +4,12 @@ import GridPostList from './GridPostList';
 
 type SearchResultsProps = {
     isSearchFetching: boolean;
-    searchedPosts: Models.Document[];
+    searchedPosts: Models.Document;
 }
 
 const SearchResults = ({ isSearchFetching, searchedPosts }: SearchResultsProps) => {
     if(isSearchFetching) return <Loader />
-
+    
     if(searchedPosts && searchedPosts.documents.length > 0) {
         return (
     <GridPostList posts={searchedPosts.documents}/>
